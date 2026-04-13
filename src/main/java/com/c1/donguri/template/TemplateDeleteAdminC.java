bf8af8fd1351e75e60c0b7cbdf876f16f9bd398f
lhm <lhm6507@gmail.com>
2026-04-13 15:01:12 +0900

    admin 로직 구현

--- src/main/java/com/c1/donguri/template/TemplateDeleteAdminC.java
+++ src/main/java/com/c1/donguri/template/TemplateDeleteAdminC.java
@@ -1,5 +1,8 @@
 package com.c1.donguri.template;
 
+import com.c1.donguri.user.UserDAO;
+import com.c1.donguri.user.UserDTO;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +14,20 @@ import java.io.IOException;
 public class TemplateDeleteAdminC extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        boolean isLoggedIn = UserDAO.USER_DAO.loginCheck(request);
 
+        if (!isLoggedIn) {
+            response.sendRedirect("login");
+            return;
+        }
+        UserDTO user = (UserDTO) request.getSession().getAttribute("user");
+
+        boolean isAdmin = UserDAO.USER_DAO.isAdmin(user.getEmail());
+
+        if (!isAdmin) {
+            response.sendRedirect("main");
+            return;
+        }
         TemplateDAO.TEMPLATE_DAO.deleteTemplate(request);
 
         response.sendRedirect("/template-list-admin");
--- src/main/java/com/c1/donguri/user/UserDTO.java
+++ src/main/java/com/c1/donguri/user/UserDTO.java
@@ -15,6 +15,8 @@ public class UserDTO {
     private String nickname;
     private String password;
     private String profileImgUrl;
+    private String roll;        // 🚨 새로 추가: 권한 (ADMIN 또는 MEMBER)
+    private Date omikujiAt;     // 🚨 새로 추가: 뽑기(오미쿠지) 한 날짜/시간
     private Date createdAt;
     private Date updatedAt;
     private String isDeleted;
